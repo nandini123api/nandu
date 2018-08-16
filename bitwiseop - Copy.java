cbb4a0183033388df8b7604906b19f6a04b4e558
nandini123api <42431825+nandini123api@users.noreply.github.com>
2018-08-16 12:32:58 +0530

    Add files via upload

---
+++ bitwiseop - Copy.java
@@ -0,0 +1,20 @@
+public class bitwiseop{
+public static void main(String[] args){
+int a=10;
+System.out.println(a<<1);
+System.out.println(a>>1);
+System.out.println(a>>1==6);
+System.out.println(a>>2==40);
+System.out.println(10>>2);
+System.out.println(10>>4);
+System.out.println(10<<3);
+System.out.println(20<<2);
+System.out.println(15>>4);
+System.out.println(12<<2);
+System.out.println(12>>2);
+System.out.println(12>>1);
+System.out.println(a>>>2);
+System.out.println(a>>1);
+System.out.println(a>>3);
+}
+}
